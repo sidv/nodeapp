fa2763905c0ea98fd68d790191fdf9133fd97dc3
sid <sid0308v@gmail.com>
2021-08-27 15:25:05 +0530

    User management

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 require('dotenv').config();
 var app = express()
 
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
+var users=[]
+
+app.put("/changeemail",(req,res) =>{
+        users.forEach((item) =>{
+                if(item.id == req.body.id){
+                        item.email = req.body.email
+                }
+        })
+        res.send("Updated")
+})
+
+
+app.put("/changeusername",(req,res) =>{
+	users.forEach((item) =>{
+		if(item.id == req.body.id){
+			item.username = req.body.username
+		}
+	})
+	res.send("Updated")
+})
+
+
+app.post("/getuserbyid",(req,res) =>{
+	user=users.filter((item) =>{	if (item.id == req.body.id) {return true} else{return false }})
+	res.send(JSON.stringify(user))
+//	res.send("User details")
+})
+
+
+app.post("/getuserbyname",(req,res) =>{
+        user=users.filter((item) =>{    if (item.name == req.body.name) {return true} else{return false }})
+        res.send(JSON.stringify(user))
+//      res.send("User details")
+})
+
+app.post("/delete",(req,res) =>{
+        user=users.filter((item) =>{    if (item.id != req.body.id) {return true} else{return false}})
+	users = user
+	res.send("User Deleted")
+})
+
+
+app.post("/register",(req,res) =>{
+	console.log(req.body)
+	users.push(req.body)
+	res.send("Registration successful")
+})
 
 app.get("/",(req,res) =>{
 	res.send("hello");
