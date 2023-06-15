162142fb0f199cdeb7b6a95165233b4b9e1bd368
sid <sid0308v@gmail.com>
2023-06-15 10:34:21 +0530

    Port changed to 8000

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.get("/",(req,res) =>{
 	res.send("hello");
 })
 
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 8000;
 
 app.listen(PORT,()=>{
 	console.log(`Server running at ${PORT}`);
